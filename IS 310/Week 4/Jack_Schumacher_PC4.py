5027c6a2f1830f8cc1d59463172b9d7260b33e23
Jack Schumacher <jackrschumacher@gmail.com>
2025-10-01 15:40:40 -0500

    Clean file to restart and optimize

--- IS 310/Week 4/Jack_Schumacher_PC4.py
+++ IS 310/Week 4/Jack_Schumacher_PC4.py
@@ -96,6 +96,102 @@ class Humanoid:
                     self.eye_color,
                 )
 
+        def set_strength(self, strength):
+            if strength >= 1 and strength <= 18:
+                self.strength = strength
+            else:
+                self.strength = random.randint(1, 18)
+                print(
+                    "You have entered a strength value outside of the range of 1 to 18. Your strength has been set at",
+                    self.strength,
+                )
+
+        def set_constitution(self, constitution):
+            if constitution >= 1 and constitution <= 18:
+                self.constitution = constitution
+            else:
+                self.constitution = random.randint(1, 18)
+                print(
+                    "You have entered a constitution value outside of the range of 1 to 18. Your constitution has been set at",
+                    self.constitution,
+                )
+
+        def set_intelligence(self, intelligence):
+            if intelligence >= 1 and intelligence <= 18:
+                self.intelligence = intelligence
+            else:
+                self.intelligence = random.randint(1, 18)
+                print(
+                    "You have entered a intelligence value outside of the range of 1 to 18. Your intelligence has been set at",
+                    self.intelligence,
+                )
+
+        def set_dexterity(self, dexterity):
+            if dexterity >= 1 and dexterity <= 18:
+                self.dexterity = dexterity
+            else:
+                self.dexterity = random.randint(1, 18)
+                print(
+                    "You have selected a dexterity value that is not between 1 and 18. Your dexterity has been set at: ",
+                    self.dexterity,
+                )
+
+        def set_wisdom(self, wisdom):
+            if wisdom >= 1 and wisdom <= 18:
+                self.wisdom = wisdom
+            else:
+                self.wisdom = random.randint(1, 18)
+                print(
+                    "You have selected a wisdom value that is not between 1 and 18. Your wisdom has been set at: ",
+                    self.wisdom,
+                )
+
+        def set_charisma(self, charisma):
+            if charisma >= 1 and charisma <= 18:
+                self.charisma = charisma
+            else:
+                self.charisma = random.randint(1, 18)
+                print(
+                    "You have selected a charisma value that is not between 1 and 18. Your charisma has been set at: ",
+                    self.charisma,
+                )
+
+        def combat():
+            print("Test")
+
+        def level_up():
+            print("Test")
+
+class Human(Humanoid):
+    print("Test")
+
+class Elf(Humanoid):
+    print("x")
+class Dwarves(Humanoid):
+    print("x")
+
+def character_creation():
+    print("Please select a character from the following list: \n 1.Human \n 2.Elf \n 3. Dwarves")
+    character_Choice_Number = input("Please enter the number corresponding")
+    if(character_Choice_Number >=1 and character_Choice_Number <= 3):
+        if(character_Choice_Number == 1):
+            default_attributes()
+            print("As a human, you can add 2 points to an attribute of your choosing. Select from any of the following attributes:")
+        elif(character_Choice_Number ==2):
+            print("As an elf")
+            default_attributes()
+        else:
+            default_attributes()
+
+    else:
+        print("The number that you entered was not valid. Please try again")
+        character_creation()
+
+    def default_attributes():
+
+    
+
 
 def main():
     print("RPG Simulator")
+    character_creation()
